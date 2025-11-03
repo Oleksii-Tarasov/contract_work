d62509ecff2ed0a60f29cf9987f8118f8be19fe9
Oleksii-Tarasov <alekseytarasov85@gmail.com>
2025-11-03 15:03:52 +0200

    Added AppContextListener.

--- src/main/java/ua/gov/court/supreme/contractwork/servlet/AppContextListener.java
+++ src/main/java/ua/gov/court/supreme/contractwork/servlet/AppContextListener.java
@@ -1,8 +1,19 @@
 package ua.gov.court.supreme.contractwork.servlet;
 
+import ua.gov.court.supreme.contractwork.service.WorkInspector;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 @WebListener
 public class AppContextListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("Server started – initializing...");
+        ServletContext servletContext = sce.getServletContext();
+        WorkInspector workInspector = new WorkInspector();
+        servletContext.setAttribute("workInspector", workInspector);
+    }
 }
